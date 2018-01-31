e8b845561997fd577a72da1c6262f9e84c6033cc
Tommy Yu <tommy.yu@auckland.ac.nz>
2018-01-31 17:34:48 +1300

    So that ACPI events may work better.
    - Some VM backends do provide this to allow more common VM management
      methods.
    - Include sys-power/acpid as part of the default installation.

--- gentoo_files/install.sh
+++ gentoo_files/install.sh
@@ -4,7 +4,7 @@ emerge --update --deep --newuse @world
 emerge app-admin/syslog-ng app-portage/gentoolkit net-misc/dhcpcd \
     sys-apps/mlocate sys-block/parted sys-boot/grub:2 \
     sys-kernel/genkernel-next sys-kernel/gentoo-sources sys-process/cronie \
-    dev-vcs/git
+    dev-vcs/git sys-power/acpid
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 eselect locale set en_US.utf8
@@ -17,6 +17,7 @@ echo -e "$(blkid | grep sda2 | cut -f2 -d\ )\t\t/\t\text4\trw,noatime\t0 1" \
     >> /etc/fstab
 ln -s net.lo /etc/init.d/net.eth0
 ln -s net.lo /etc/init.d/net.eth1
+rc-update add acpid default
 rc-update add net.eth0 default
 rc-update add syslog-ng default
 rc-update add cronie default
